2ee0480859ed1b169afab32ea68c6e2ce176bd77
Anna Grygierczyk <anna.grygierczyk@wp.pl>
2024-06-11 19:52:42 +0200

    dodawanie spothan

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -1,55 +1,68 @@
-import {useState} from "react";
+import { useState } from "react";
 import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
 
-export default function MeetingsPage({username}) {
-    const [meetings, setMeetings] = useState([]);
-    const [addingNewMeeting, setAddingNewMeeting] = useState(false);
+export default function MeetingsPage({ username }) {
+  const [meetings, setMeetings] = useState([]);
+  const [addingNewMeeting, setAddingNewMeeting] = useState(false);
 
-    function handleNewMeeting(meeting) {
-        const nextMeetings = [...meetings, meeting];
-        setMeetings(nextMeetings);
-        setAddingNewMeeting(false);
+  async function handleNewMeeting(meeting) {
+    const response = await fetch("/api/meetings", {
+      method: "POST",
+      body: JSON.stringify(meeting),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      const nextMeetings = [...meetings, meeting];
+      setMeetings(nextMeetings);
+      setAddingNewMeeting(false);
     }
+  }
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
-    }
+  function handleDeleteMeeting(meeting) {
+    const nextMeetings = meetings.filter((m) => m !== meeting);
+    setMeetings(nextMeetings);
+  }
 
-    function handleSignIn(meeting) {
-        const nextMeetings = meetings.map(m => {
-            if (m === meeting) {
-                m.participants = [...m.participants, username];
-            }
-            return m;
-        });
-        setMeetings(nextMeetings);
-    }
+  function handleSignIn(meeting) {
+    const nextMeetings = meetings.map((m) => {
+      if (m === meeting) {
+        m.participants = [...m.participants, username];
+      }
+      return m;
+    });
+    setMeetings(nextMeetings);
+  }
 
-    function handleSignOut(meeting) {
-        const nextMeetings = meetings.map(m => {
-            if (m === meeting) {
-                m.participants = m.participants.filter(u => u !== username);
-            }
-            return m;
-        });
-        setMeetings(nextMeetings);
-    }
+  function handleSignOut(meeting) {
+    const nextMeetings = meetings.map((m) => {
+      if (m === meeting) {
+        m.participants = m.participants.filter((u) => u !== username);
+      }
+      return m;
+    });
+    setMeetings(nextMeetings);
+  }
 
-    return (
-        <div>
-            <h2>Zajęcia ({meetings.length})</h2>
-            {
-                addingNewMeeting
-                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
-                    : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
-            }
-            {meetings.length > 0 &&
-                <MeetingsList meetings={meetings} username={username}
-                              onDelete={handleDeleteMeeting}
-                              onSignIn={handleSignIn}
-                              onSignOut={handleSignOut}/>}
-        </div>
-    )
+  return (
+    <div>
+      <h2>Zajęcia ({meetings.length})</h2>
+      {addingNewMeeting ? (
+        <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)} />
+      ) : (
+        <button onClick={() => setAddingNewMeeting(true)}>
+          Dodaj nowe spotkanie
+        </button>
+      )}
+      {meetings.length > 0 && (
+        <MeetingsList
+          meetings={meetings}
+          username={username}
+          onDelete={handleDeleteMeeting}
+          onSignIn={handleSignIn}
+          onSignOut={handleSignOut}
+        />
+      )}
+    </div>
+  );
 }
